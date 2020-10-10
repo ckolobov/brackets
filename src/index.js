dc50d737b35512d930ab6d54d735e41b71a54e38
Constantine Kolobov <kolobovconstantine@gmail.com>
2020-10-10 13:44:35 +0300

    feat: solve brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const openBrackets = {};
+  const closeBrackets = {};
+
+  for (let i = 0; i < bracketsConfig.length; i++) {
+    openBrackets[bracketsConfig[i][0]] = true;
+    closeBrackets[bracketsConfig[i][1]] = bracketsConfig[i][0];
+  }
+
+  const bracketsStack = [];
+  let result = true;
+
+  for (let j = 0; j < str.length; j++) {
+    if (closeBrackets.hasOwnProperty(str[j])) {
+      const lastOpen = bracketsStack[bracketsStack.length - 1];
+      if (lastOpen === closeBrackets[str[j]]) {
+        bracketsStack.pop();
+      } else if (openBrackets.hasOwnProperty(str[j])) {
+        bracketsStack.push(str[j]);
+      } else {
+        result = false;
+        break;
+      }
+    } else if (openBrackets.hasOwnProperty(str[j])) {
+        bracketsStack.push(str[j]);
+    }
+  }
+
+  if (bracketsStack.length > 0) {
+    result = false;
+  }
+
+  return result;
 }
